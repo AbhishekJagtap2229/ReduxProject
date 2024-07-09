d64a9a66c09f6b39ef23151daac53844f12fcc6d
Abhishek Subhash Jagtap <abhishekjagtap022@gmail.com>
2024-07-09 09:48:46 +0530

    done

--- src/redux/reducers/counter.js
+++ src/redux/reducers/counter.js
@@ -1,3 +1,14 @@
+import { ReducerType } from "@reduxjs/toolkit"
+
 const initilstate = {
     value:0
 }
+
+const counterSlice = createSlice ({
+    name:'counter',
+    initilstate:aanda,
+    Reducers:{
+        
+    }
+
+})
