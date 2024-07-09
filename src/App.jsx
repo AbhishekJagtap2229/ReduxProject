40388254d7ec30e7bbe755088cde1524fd67b683
Abhishek Subhash Jagtap <abhishekjagtap022@gmail.com>
2024-07-09 09:35:51 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import Todo from'./Compo/TodoList'
+import Temp from './Compo/TemperatureConverter'
+import Search from './Compo/SearchAndFilter'
+import Shopping from './Compo/ShoppingCart'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        {/* <Todo></Todo>
+        <br></br>
+        <Temp></Temp>
+        <br/>
+        <Search/>
+        <br/>
+        <Shopping/> */}
+      </div>
+      
+    </>
+  )
+}
+
+export default App
